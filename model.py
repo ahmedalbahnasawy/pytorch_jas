a0136c9a122acac1a18a99322930b04367c64c00
ahmedalbahnasawy <50653875+ahmedalbahnasawy@users.noreply.github.com>
2020-05-31 14:52:07 +0400

    Update model.py

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch.nn.functional as F
 import torch
 import torch.nn
+torch.manual_seed(1)
 class STT(torch.nn.Module):
   def __init__(self , input_channel):
     super(STT, self).__init__()
@@ -14,7 +15,10 @@ class STT(torch.nn.Module):
     self.bn_3 = torch.nn.BatchNorm2d(128, eps=1e-8, momentum=0.1,affine=True, track_running_stats=True)
     self.conv_4 = torch.nn.Conv2d(128,128,kernel_size=3, stride=1, padding=1)
     self.bn_4 = torch.nn.BatchNorm2d(128,eps=1e-8, momentum=0.1, affine=True, track_running_stats=True)
-
+    
+    self.Encoder = RNNEncoder(rnn_tybe,input_dim,num_units, num_layer,dropout_in)
+    self.Decoder = RNNDecoder()
+    self.loss = Build_loss(args.model)
 
 
     def forward(self, batch_input, input_length, **kwargs):
@@ -26,27 +30,36 @@ class STT(torch.nn.Module):
       x = F.relu(self.bn_3(self.conv_3(x)))
       x = F.relu(self.bn_4(self.conv_4(x)))
       x = F.max_pool2d(x,2,stride=2,ceil_mode=True)
+      
+      
 
       return 
-input_b = torch.randn(83)
-model = STT(input_b)
 
-class Encoder(torch.nn.Module):
-  def __init__ ():
-    super(Encoder).__()
+
+class RNNEncoder(torch.nn.Module):
+  def __init__ (self, rnn_tybe,input_dim,num_units, num_layer,dropout_in ):
+    
+    super(RNNEncoder).__()
     "Not yet implemented "
 class Decoder(torch.nn.Module):
   def __init__():
-    super(Decoder).__init__()
+    super(RNNDecoder).__init__()
     "Not yet implemented "
 
-class Transducer(torch.nn.Module):
-  def __init__():
-    super(Transducer).__init__()
-    "Not yet implemented "
+class Build_loss(torch.nn.Module):
+  def __init__(self, loss_tybe, loss_list):
 
-class CTC(torch.nn.Module):
-  def __init__():
-    super(CTC).__init__()
-    "Not yet implemented "
+    super(Build_loss).__init__()
+        
+    if loss_type not in loss_list:
+      raise NotImplementedError
+    else:
+      if loss_type = 'transducer':
+        from warprnnt_pytorch import RNNTLoss
+        criterion = RNNTloss()
+      else:
+        criterion = torch.nn.CTCLoss(reduction='none', zero_infinity=True)
 
+
+input_b = torch.randn(83)
+model = STT(input_b)
